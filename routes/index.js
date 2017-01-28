eb05c05339cb0572865f7a0706101a3510e32a47
Tony Bogun <antonybogun@gmail.com>
2017-01-28 18:55:01 -0500

    Update root page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  let currentDate = new Date();
+  res.render('index', { 
+    title: 'Lab 1',
+    date :  currentDate.toDateString()
+  });
 });
 
 module.exports = router;
