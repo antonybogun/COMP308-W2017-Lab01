c9ee078dc281f92a3efe89ddc525c1c22c17dc62
Tony Bogun <antonybogun@gmail.com>
2017-01-28 22:52:14 -0500

    Add contact, about pages

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,28 @@ var router = express.Router();
 router.get('/', (req, res, next) => {
   let currentDate = new Date();
   res.render('index', { 
-    title: 'Lab 1',
+    title: 'Lab 1 - Home',
     date :  currentDate.toDateString()
+  });
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('about', { 
+    title: 'Lab1 - About',
+    textSubtitle: "About Text",
+    imageSubtitle: "About Image",
+    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    image:"Assets/images/about-us.jpg"
+  });
+});
+
+router.get('/contact', (req, res, next) => {
+  res.render('contact', { 
+    title: 'Lab1 - Contact',
+    textSubtitle: "Contact Text",
+    imageSubtitle: "Contact Image",
+    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    image:"Assets/images/contact-us.jpg"
   });
 });
 
